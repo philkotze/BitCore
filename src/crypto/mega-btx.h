82523c6e249bf93bf6aac9075a2192daf1bed642
flexmeister <>
2020-09-05 14:03:49 +1000

    Test13

--- src/crypto/mega-btx.h
+++ src/crypto/mega-btx.h
@@ -130,10 +130,10 @@ inline uint256 Mega_Btx(const T1 pbegin, const T1 pend,uint32_t timestamp)
 			            for (int i = 0; i < 8; i++)
                 std::cout << "p1 = " << permutation_1[i] << '\n';
 
-            for (int i = 0; i < 16; i++)
+            for (int i = 8; i < 16; i++)
                 std::cout << "p2 = " << permutation_2[i] << '\n';
 
-            for (int i = 0; i < 23; i++)
+            for (int i = 16; i < 23; i++)
                 std::cout << "p3 = " << permutation_3[i] << '\n';
 
             int lenToHash = 64;
