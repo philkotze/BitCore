e0197cb0a9e45143ff98de2aa98a9413a875dcf0
flexmeister <>
2020-09-11 18:17:06 +1000

    Test22

--- src/crypto/mega-btx.h
+++ src/crypto/mega-btx.h
@@ -158,7 +158,7 @@ inline uint256 Mega_Btx(const T1 pbegin, const T1 pend,uint32_t timestamp)
 			printf("pBegin: %d", pbegin[0]);
 			
 			printf("hashinit:\n");
-			debuglog_hex(toHashinit, 64);
+			debuglog_hex(toHashinit[0], 64);
 
 
             sph_blake512_init(&ctx_blake);
